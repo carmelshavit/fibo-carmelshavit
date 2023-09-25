38525ab1de0134eb8f14e1a80b1b472848cfe3c1
Carmel <140419915+carmelshavit@users.noreply.github.com>
2023-09-25 19:16:33 +0300

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+// let num = [0, 1, 1 , 2, 3, 5, 8, 13];
+function fiboCalc() {
+    const index = document.querySelector(".num-input").value
+    let prev = 0;
+    let curr = 1;
+    for (let i = 0; i <= index; i++) {
+        const next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+    document.querySelector(".result").innerHTML = curr
+}
+fiboCalc();
+
+
+
